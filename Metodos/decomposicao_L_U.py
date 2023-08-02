6726cad108056a976ea9e6f66db9500333019b58
ErikMarques <lucro.alternativo@outlook.com>
2023-08-02 00:59:06 -0300

    método que Implementa a Decomposição LU

--- Metodos/decomposicao_L_U.py
+++ Metodos/decomposicao_L_U.py
@@ -1,76 +1,74 @@
 from eliminacao_gauss import eliminacao_gauss
 from numpy import eye, subtract, zeros
+from typing import List
 
-def _triangular_superior(U: list[list[float]], b: list[float]) -> list[float]:
-   '''
-    U: matriz triangular superior
-    b: termos independentes do sistema triangular superior
-
-    retorna a solução do sistema
+def _triangular_superior(U: List[List[float]], b: List[float]) -> List[float]:
     '''
-   tamanho = len(U)
+    Função que resolve o sistema Ux = y.
 
-   x = zeros(tamanho)
-   x[tamanho-1] = b[tamanho-1] / U[tamanho-1][tamanho-1] # ultimo x[j]
+    Args:
+        U: Matriz triangular superior.
+        b: Termos independentes do sistema triangular superior.
 
-   #range(inicio,parada,passo)
-   for k in range(tamanho-2,-1,-1):
-      for t in range(k,tamanho):
-         b[k] = b[k] - (U[k][t] * x[t])
+    Returns:
+        solucao: Vetor solução do sistema.
+    '''
+    tamanho = len(U)
+    solucao = zeros(tamanho)
+    solucao[tamanho-1] = b[tamanho-1] / U[tamanho-1][tamanho-1] # Último solucao[j]
 
-      x[k] = b[k] / U[k][k]
+    for k in range(tamanho-2, -1, -1):
+        for t in range(k, tamanho):
+            b[k] = b[k] - (U[k][t] * solucao[t])
 
-   return x
+        solucao[k] = b[k] / U[k][k]
 
-def _triangular_inferior(L: list[list[float]], b: list[float]) -> list[float]:
-   '''
-   A: matriz triangular inferior
-   b: termos independentes do sistema triangular inferior
+    return solucao
 
-   retorna a solução do sistema
-   '''
-   tamanho = len(L)
+def _triangular_inferior(L: List[List[float]], b: List[float]) -> List[float]:
+    '''
+    Função que resolve o sistema Ly = b.
 
-   x = zeros(tamanho)
-   x[0] = b[0] # primeiro x[j]
+    Args:
+        L: Matriz triangular inferior.
+        b: Termos independentes do sistema triangular inferior.
 
-   for k in range(1,tamanho-1):
-      for t in range(0,k-1):
-         b[k] = b[k] - (L[k][t] * x[t])
+    Returns:
+        solucao: Vetor solução do sistema.
+    '''
+    tamanho = len(L)
+    solucao = zeros(tamanho)
+    solucao[0] = b[0] # Primeiro solucao[j]
 
-   x = b
+    for k in range(1, tamanho-1):
+        for t in range(0, k-1):
+            b[k] = b[k] - (L[k][t] * solucao[t])
 
-   return x
+    solucao = b
 
-def decomposicao_LU(matriz: list[list[float]], matriz_independente: list[float]) -> list[float]:
+    return solucao
+
+def decomposicao_LU(matriz: List[List[float]], matriz_independente: List[float]) -> List[float]:
     '''
     Aplica o método da decomposição LU a uma matriz quadrada fornecida.
 
     Args:
-        matriz(list): matriz quadrada que se deseja decompor, também chamda de matriz dos coeficientes do sistema.
-        matriz_independente(list): matriz dos termos independentes (soluções do sistema)
+        matriz (list[list[float]]): 
+            Matriz quadrada que se deseja decompor, também chamada de matriz dos coeficientes do sistema.
+            
+        matriz_independente (list[float]): Matriz dos termos independentes (soluções do sistema).
 
     Returns:
-        list: solução do sistema.
+        solucao (list[float]): Vetor solução do sistema.
     '''
-    solucao: list[float]
-
-    # obtendo as matrizes U (escalonada) e a matriz m, dos fatores do escalonamento.
-    matrizes = eliminacao_gauss(matriz) 
-    
-    #separando as matrizes U e m
+    matrizes = eliminacao_gauss(matriz)     # Obtendo as matrizes U (escalonada) e a matriz m, dos fatores do escalonamento.
     matriz_U = matrizes[0] 
-    fatores = matrizes[1]  
-    
-    # criando uma matriz identidade com mesmas dimensões de U
-    matriz_L = eye(len(matriz_U)) 
-    
-    # completando com os fatores do escalonamento
-    matriz_L = subtract(matriz_L,fatores) 
-   
-    # Ly = b
-    y = _triangular_inferior(matriz_L,matriz_independente)
-    # Ux = y
-    solucao = _triangular_superior(matriz_U,y)
-    
+    fatores = matrizes[1] 
+
+    matriz_L = eye(len(matriz_U))           # Criando uma matriz identidade com as mesmas dimensões de U
+    matriz_L = subtract(matriz_L, fatores)  # Completando com os fatores do escalonamento
+
+    y = _triangular_inferior(matriz_L, matriz_independente) # Ly = b
+    solucao = _triangular_superior(matriz_U, y) # Ux = y
+
     return solucao
